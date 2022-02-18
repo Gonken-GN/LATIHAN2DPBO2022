a68ac9a06ac4ff759a1057715a06d891b4a86066
RFadhil <58224930+Gonken-GN@users.noreply.github.com>
2022-02-18 16:07:41 +0700

    Update hardware.java

--- Lat1/hardware.java
+++ Lat1/hardware.java
@@ -1,7 +1,11 @@
+//child class dari parent Product
 public class hardware extends product{
+    //Attribute private
     private String brand;
     private String model;
+    //constructor
     hardware(){}
+    //get & set untuk tiap private
     public String getBrand() {
         return brand;
     }
